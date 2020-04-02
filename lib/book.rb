56e244064bf65b57898242fd46eb587240a1843c
devWRM <mcwen@comcast.net>
2020-04-02 18:56:03 -0400

    Done.

--- lib/book.rb
+++ lib/book.rb
@@ -3,41 +3,44 @@
 
 class Book
 
+    attr_accessor :author, :page_count, :genre
+    attr_reader :title
+
     def initialize(title)
         @title = title
     end
 
-    def title=(title)
-        @title = title
-    end
+#    def title=(title)
+#        @title = title
+#    end
 
-    def title
-        @title
-    end
+#    def title
+#        @title
+#    end
 
-    def author=(author)
-        @author = author
-    end
+#    def author=(author)
+#        @author = author
+#    end
 
-    def author
-        @author
-    end
+#    def author
+#        @author
+#    end
 
-    def page_count=(pages)
-        @page_count = pages
-    end
+#    def page_count=(pages)
+#        @page_count = pages
+#    end
 
-    def page_count
-        @page_count
-    end
+#    def page_count
+#        @page_count
+#    end
 
-    def genre=(genre)
-        @genre = genre
-    end
+#    def genre=(genre)
+#        @genre = genre
+#    end
 
-    def genre
-        @genre
-    end
+#    def genre
+#        @genre
+#    end
 
     def turn_page
         puts "Flipping the page...wow, you read fast!"
